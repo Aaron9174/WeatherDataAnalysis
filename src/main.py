2efad301c24cbdfba6f0414646313e0648b704dc
Aaron Hebson <aarommh7@gmail.com>
2024-10-19 15:51:26 -0400

    Using meteostat to retrieve historical weather data

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import meteostat
+from datetime import datetime
+
+# Define the geographical point
+point = meteostat.Point(28.03, -80.59)  # Palm Bay, FL
+
+# Set the start and end dates
+start = datetime(1945, 7, 1)
+end = datetime(1945, 7, 31)
+
+# Fetch daily weather data
+data = meteostat.Daily(point, start, end)
+data = data.fetch()
+
+print(data)
